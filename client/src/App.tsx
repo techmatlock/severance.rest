6a7f41fcd4f2e16cbc2eebd13de0769e6e801a53
Mark Matlock <techmatlock@gmail.com>
2025-02-13 18:40:57 -0800

    Added JSX

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <main className="flex h-full">Some content</main>;
+  return (
+    <main className="grid">
+      <div className="mt-12 text-center">
+        <h2 className="text-3xl font-semibold">severance.rest</h2>
+        <p className="mt-2">A free REST API for random Severance quotes</p>
+      </div>
+      <div className="flex items-center justify-between mx-auto mt-8">
+        <div className="mr-20">
+          <a href="https://api.severance.rest" target="_blank" className="text-blue-500 hover:underline">
+            https://api.severance.rest
+          </a>
+        </div>
+        <div className="flex gap-4">
+          <button className="px-4 py-2 bg-gray-200 rounded hover:bg-gray-600">tweet</button>
+          <button className="px-4 py-2 bg-gray-200 rounded hover:bg-gray-600">refresh</button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
