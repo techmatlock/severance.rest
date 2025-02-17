4b6bdd981d5f6cd8b67b5f8905a169d532a2c351
Mark Matlock <techmatlock@gmail.com>
2025-02-16 16:41:16 -0800

    Removed provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { QuotesProvider } from "./context/QuotesContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QuotesProvider>
-      <App />
-    </QuotesProvider>
+    <App />
   </StrictMode>
 );
