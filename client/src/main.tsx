f9874b5ba8d922c2734f060f02d2bfc313a67ba0
Mark Matlock <techmatlock@gmail.com>
2025-02-26 21:36:51 -0800

    Added BrowserRouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { QuotesProvider } from "./context/QuotesContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QuotesProvider>
-      <App />
-    </QuotesProvider>
+    <BrowserRouter>
+      <QuotesProvider>
+        <App />
+      </QuotesProvider>
+    </BrowserRouter>
   </StrictMode>
 );
