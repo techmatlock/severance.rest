cbed1eb23f62d87344f01f5d66d22a59b5b63b19
Mark Matlock <techmatlock@gmail.com>
2025-02-27 14:28:32 -0800

    Fixed dynamodb put_item using correct argument syntax

--- lambdas/lambda_function.py
+++ lambdas/lambda_function.py
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
             name = event.get('queryStringParameters', {}).get('name')
             response = get_quotes_by_name(name)
         elif http_method == 'POST' and path == quote_path:
-            post = json.loads(event.get('body', '{}'))
+            post = json.loads(event['body'])
             response = add_quote(post)
         else:
             response = build_response(404, '404 Not Found')
@@ -41,9 +41,9 @@ def get_quotes():
         return build_response(400, e.response['Error']['Message'])
     
 def get_quotes_by_name(name):
-    if not name:
-        return build_response(400, 'Missing query parameter: name')
     try:
+        if not name:
+            return build_response(400, 'Missing query parameter: name')
         response = dynamodb_table.scan(FilterExpression=Attr('name').begins_with(name))
         items = response.get('Items', [])
         return build_response(200, items)
@@ -52,25 +52,20 @@ def get_quotes_by_name(name):
         return build_response(400, e.response['Error']['Message'])
     
 def add_quote(data):
-    if 'name' not in data or 'quote' not in data:
-            return build_response(400, {"message": "Missing name or quote in request body"})
     try:
-        response = dynamodb_table.scan(
-            TableName=dynamodb_table,
-            Select="COUNT"
-        )
+        if 'name' not in data or 'quote' not in data:
+                return build_response(400, {"message": "Missing name or quote in request body"})
 
-        new_id = str(response['Count'] + 1)  # Incrementing PK "quoteId" + 1
+        response = dynamodb_table.scan()
 
         dynamodb_table.put_item(
-            TableName=dynamodb_table,
             Item={
-                'quoteId': {'N': new_id},
-                'name': {'S': data['name']},
-                'quote': {'S': data['quote']}
+                'quoteId': str(len(response['Items']) + 1), 
+                'name': data['name'],
+                'quote': data['quote']
             }
         )
-        return build_response(201, {"message": "Quote added successfully", "quoteId": new_id})
+        return build_response(201, {"message": "Quote added successfully"})
     except ClientError as e:
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
