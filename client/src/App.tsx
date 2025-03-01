873236104d45b44d1e3fa9e229cbba2715513b98
Mark Matlock <techmatlock@gmail.com>
2025-02-28 17:30:47 -0800

    Add protected route for dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
+import ProtectedRoutes from "./pages/ProtectedRoutes";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
